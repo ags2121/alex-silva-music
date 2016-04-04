722c2119af44dff587893c8dffdcadf8fa2560e5
ags2121 <alexsilva88@gmail.com>
2016-04-04 17:49:58 -0400

    add bio styling

--- src/cljs/alex_silva_music/views.cljs
+++ src/cljs/alex_silva_music/views.cljs
@@ -197,7 +197,7 @@
    [:hr]
    [panels db/panels]
    [panel :projects (projects db/projects)]
-   [panel :bio (fn [] [:div.bio "Alex Silva is dope."])]
+   [panel :bio (fn [] [:div.bio-text "Alex Silva is dope."])]
    [panel :links links-component]
    [panel :favorites favorites-component]
    [:img.alex {:src  "/assets/alex-studio.png"}]
