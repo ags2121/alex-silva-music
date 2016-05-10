2034f743ac74bfd5224cb12a0e50a4479128859f
ags2121 <alexsilva88@gmail.com>
2016-05-10 10:32:00 -0400

    fix class not found errors

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject alex-silva-music "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
                  [ring "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
                  [bk/ring-gzip "0.1.1"]
@@ -8,10 +8,10 @@
                  [compojure "1.4.0"]
                  [environ "1.0.2"]
                  [reagent "0.5.1"]
-                 [re-frame "0.6.0"]
+                 [re-frame "0.6.0" :exclusions [org.clojure/clojurescript]]
                  [secretary "1.2.3"]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
+  :plugins [[lein-cljsbuild "1.1.0"]
             [lein-environ "1.0.1"]]
 
   :min-lein-version "2.5.3"
@@ -33,16 +33,16 @@
               {:app
                {:source-paths ["src/cljs"]
 
-                :figwheel true
+                :figwheel     true
                 ;; Alternatively, you can configure a function to run every time figwheel reloads.
                 ; :figwheel {:on-jsload "alex-silva-music.core/mount-root"}
 
 
-                :compiler {:main alex-silva-music.core
-                           :asset-path "js/compiled/out"
-                           :output-to "resources/public/js/compiled/app.js"
-                           :output-dir "resources/public/js/compiled/out"
-                           :source-map-timestamp true}}}}
+                :compiler     {:main                 alex-silva-music.core
+                               :asset-path           "js/compiled/out"
+                               :output-to            "resources/public/js/compiled/app.js"
+                               :output-dir           "resources/public/js/compiled/out"
+                               :source-map-timestamp true}}}}
 
   ;; When running figwheel from nREPL, figwheel will read this configuration
   ;; stanza, but it will read it without passing through leiningen's profile
@@ -52,14 +52,14 @@
   :figwheel {;; :http-server-root "public"       ;; serve static assets from resources/public/
              ;; :server-port 3449                ;; default
              ;; :server-ip "127.0.0.1"           ;; default
-             :css-dirs ["resources/public/css"]  ;; watch and update CSS
+             :css-dirs       ["resources/public/css"]       ;; watch and update CSS
 
              ;; Instead of booting a separate server on its own port, we embed
              ;; the server ring handler inside figwheel's http-kit server, so
              ;; assets and API endpoints can all be accessed on the same host
              ;; and port. If you prefer a separate server process then take this
              ;; out and start the server with `lein run`.
-             :ring-handler alex-silva-music.server/http-handler
+             :ring-handler   alex-silva-music.server/http-handler
 
              ;; Start an nREPL server into the running figwheel process. We
              ;; don't do this, instead we do the opposite, running figwheel from
@@ -81,54 +81,28 @@
   :doo {:build "test"}
 
   :profiles {:dev
-             {:dependencies [[figwheel "0.5.0-6"]
-                             [figwheel-sidecar "0.5.0-6"]
+             {:dependencies [[figwheel-sidecar "0.5.0" :exclusions [org.clojure/clojurescript]]
                              [com.cemerick/piggieback "0.2.1"]
                              [org.clojure/tools.nrepl "0.2.12"]]
 
-              :plugins [[lein-figwheel "0.5.0-6"]
-                        [lein-doo "0.1.6"]]
+              :plugins      [[lein-doo "0.1.6"]]
 
-              :cljsbuild {:builds
-                          {:test
-                           {:source-paths ["src/cljs" "test/cljs"]
-                            :compiler
-                                          {:output-to "resources/public/js/compiled/testable.js"
-                                           :main alex-silva-music.runner
-                                           :optimizations :none}}}}}
+              :cljsbuild    {:builds
+                             {:test
+                              {:source-paths ["src/cljs" "test/cljs"]
+                               :compiler
+                                             {:output-to     "resources/public/js/compiled/testable.js"
+                                              :main          alex-silva-music.runner
+                                              :optimizations :none}}}}}
 
              :uberjar
              {:source-paths ^:replace ["src/clj"]
-              :hooks [leiningen.cljsbuild]
-              :omit-source true
-              :aot :all
-              :cljsbuild {:builds
-                          {:app
-                           {:source-paths ^:replace ["src/cljs"]
-                            :compiler
-                                          {:optimizations :advanced
-                                           :pretty-print false}}}}}}
-
-  ;:cljsbuild {:builds [{:id           "dev"
-  ;                      :source-paths ["src/cljs"]
-  ;                      :figwheel     {:on-jsload "alex-silva-music.core/mount-root"}
-  ;                      :compiler     {:main                 alex-silva-music.core
-  ;                                     :output-to            "resources/public/js/compiled/app.js"
-  ;                                     :output-dir           "resources/public/js/compiled/out"
-  ;                                     :asset-path           "js/compiled/out"
-  ;                                     :source-map-timestamp true}}
-  ;
-  ;                     {:id           "test"
-  ;                      :source-paths ["src/cljs" "test/cljs"]
-  ;                      :compiler     {:output-to     "resources/public/js/compiled/test.js"
-  ;                                     :main          alex-silva-music.runner
-  ;                                     :optimizations :none}}
-  ;
-  ;                     {:id           "min"
-  ;                      :source-paths ["src/cljs"]
-  ;                      :compiler     {:main            alex-silva-music.core
-  ;                                     :output-to       "resources/public/js/compiled/app.js"
-  ;                                     :optimizations   :advanced
-  ;                                     :closure-defines {goog.DEBUG false}
-  ;                                     :pretty-print    false}}]}
-  )
+              :hooks        [leiningen.cljsbuild]
+              :omit-source  true
+              :aot          :all
+              :cljsbuild    {:builds
+                             {:app
+                              {:source-paths ^:replace ["src/cljs"]
+                               :compiler
+                                             {:optimizations :advanced
+                                              :pretty-print  false}}}}}})
